95f6fa031999a59f4dce41810a834d4ff6258ac2
LEEYOSEPH <laksmi0628@gmail.com>
2022-06-19 08:44:45 +0900

    회원 서비스 테스트

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -8,8 +8,14 @@ import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
+    private final MemberRepository memberRepository;
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    public MemberService(MemberRepository memberRepository) {
+
+        this.memberRepository = memberRepository;
+    }
+
 
 
     /*
